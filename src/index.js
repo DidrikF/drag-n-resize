4f09fcfda09ea8b96e2e1467ba21a0102c0f048f
DidrikF <didrik.fleischer@gmail.com>
2018-03-27 13:41:46 +0200

    Frist Commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// this is the parent
+export default class  Dragon {
+    constructor (parentElement, options) {
+        this.container = parentElement;
+        this.height = options.height || "100%";
+        this.width = options.width || "100%";
+    }
+
+    addChild () {
+
+    }
+
+    resize () {
+
+    }
+
+    move () {
+    
+    }
+    
+}
